6a502a913e356e3c61700c6af09925abb51c5565
Yakiyo <yakiyo1206@gmail.com>
2024-11-05 23:34:29 +0600

    feat: update default logger function

--- test/easy_log_test.dart
+++ test/easy_log_test.dart
@@ -7,23 +7,31 @@ import 'package:test/test.dart';
 import 'package:easy_log/easy_log.dart';
 
 void main() {
+  // in test, the first char in stderr/stdout is not printed
+  stderr.writeln("");
+  stdout.writeln("");
+  updateDefaultLogger(writer: StdOutWriter());
   test('level test', () {
     print("${Level.off.index} ${Level.error.name}");
   });
 
   test('logger test', () {
-    final l = Logger(level: Level.warn, writer: StdErrWriter());
+    final l = Logger(level: Level.warn, writer: StdOutWriter());
     l.debug("this should not be printed");
     l.error("this should be printed");
   });
 
   test('stderr printing test', () {
-    stderr
-        .writeln(""); // in test, the first char in stderr/stdout is not printed
     error("This is an error");
     warn("This is a warning");
     info("This is an info");
     debug("This is a debug");
     trace("This is a trace");
+  });
+
+  test("logger update test", () {
+    debug("This message should not be printed");
+    updateDefaultLogger(level: Level.debug);
+    debug("This message should be printed");
   });
 }
